18f2899428cca01e0537619a7311fae77515ee2d
Антон Рябов <nofacenoname3@yandex.ru>
2022-08-22 23:13:45 +0400

    first commit fourth task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,49 @@
-function Student(name, gender, age) {
-    // Ваш код
+f"use scrict"
 
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let student1 = new Student("Маша", "женщина", 21);
+let student2 = new Student("Сергей", "мужчина", 25);
+let student3 = new Student("Игнат", "мужчина", 40);
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...rest) {
+  if (this.marks === undefined) {
+    this.marks = [...rest];
+  } else {
+    this.marks.push(...rest);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  let length = this.marks.length;
+
+  for (let i = 0; i < length; i++) {
+    sum += this.marks[i];
+  }
+  return sum / length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
+}
